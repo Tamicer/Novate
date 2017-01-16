9607105aab576478630b5a4fdc9d71880780ff66
liuyongkui <liuyongkui726@pingan.com.cn>
2017-01-16 16:36:38 +0800

    增加Http取消管理机制

---
+++ novate/src/main/java/com/tamic/novate/RxApiManager.java
@@ -0,0 +1,101 @@
+/*
+ *    Copyright (C) 2016 Tamic
+ *
+ *    link :https://github.com/Tamicer/Novate
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.tamic.novate;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.util.ArrayMap;
+
+import java.util.Set;
+
+import rx.Subscription;
+
+/**
+ * Created by Tamic on 2017-01-16.
+ */
+
+public class RxApiManager implements RxActionManager<Object> {
+
+    private static RxApiManager sInstance = null;
+
+    private ArrayMap<Object, Subscription> maps;
+
+    public static RxApiManager get() {
+
+        if (sInstance == null) {
+            synchronized (RxApiManager.class) {
+                if (sInstance == null) {
+                    sInstance = new RxApiManager();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private RxApiManager() {
+        maps = new ArrayMap<>();
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    public void add(Object tag, Subscription subscription) {
+        maps.put(tag, subscription);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    public void remove(Object tag) {
+        if (!maps.isEmpty()) {
+            maps.remove(tag);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public void removeAll() {
+        if (!maps.isEmpty()) {
+            maps.clear();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override public void cancel(Object tag) {
+        if (maps.isEmpty()) {
+            return;
+        }
+        if (maps.get(tag) == null) {
+            return;
+        }
+        if (!maps.get(tag).isUnsubscribed()) {
+            maps.get(tag).isUnsubscribed();
+            maps.remove(tag);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override public void cancelAll() {
+        if (maps.isEmpty()) {
+            return;
+        }
+        Set<Object> keys = maps.keySet();
+        for (Object apiKey : keys) {
+            cancel(apiKey);
+        }
+    }
+
+}
